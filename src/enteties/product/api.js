56cbc5383a4108bb198b5ac8bb926da93d07a73f
Lxg1c <teran_2005@mail.ru>
2025-03-25 17:18:21 +0300

    initialized by fsd arch

---
+++ src/enteties/product/api.js
@@ -0,0 +1,33 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {BASE_URL_API} from "../../shared/lib/const.js";
+
+export const getProducts = createAsyncThunk(
+    'products/getProducts',
+    async (payload, thunkAPI) => {
+        try {
+            const response = await axios.get(
+                `http://localhost:8000/api/v1/product/`
+            );
+            return response.data;
+        } catch (error) {
+            console.log(error, payload);
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
+
+export const getProductById = createApi({
+    reducerPath: 'productApi',
+    baseQuery: fetchBaseQuery({ baseUrl: BASE_URL_API }),
+    tagTypes: ['Product'],
+    endpoints: (builder) => ({
+        getProduct: builder.query({
+            query: ( {id}) => `/products/${id}`,
+        }),
+    }),
+});
+
+export const { useGetProductQuery} = getProductById;
