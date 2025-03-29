94a7a807702c7ad6f365758e07f1858633d315a5
Lxg1c <teran_2005@mail.ru>
2025-03-29 22:24:58 +0300

    fixed warning

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import '../scss/index.scss';
 import { store } from './store/store.js';
 import { Provider } from 'react-redux';
 
@@ -10,7 +9,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <React.StrictMode>
-            <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+            <BrowserRouter
+                future={{
+                    v7_startTransition: true,  // Добавьте этот флаг
+                    v7_relativeSplatPath: true
+                }}
+            >
                 <App />
             </BrowserRouter>
         </React.StrictMode>
