da9241691d33a2b5aa80f615983a28cf7a457f13
Lxg1c <teran_2005@mail.ru>
2025-03-29 22:26:34 +0300

    deleted header

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import ProductList from '../../widgets/ProductList/ProductList.jsx';
 import Category from '../../features/Category/Category.jsx';
-import Header from '../../widgets/Header/Header.jsx';
 import Spinner from 'react-bootstrap/Spinner';
 
 const Home = () => {
@@ -22,33 +21,30 @@ const Home = () => {
     }, [location]);
 
     return (
-        <>
-            <Header />
-            <section className='home'>
-                <div className='home__container'>
-                    <div className='home__container-catalog' >
-                        <h2 className='home__catalog-title' id="news">Новости</h2>
-                        <Category />
+        <section className='home'>
+            <div className='home__container'>
+                <div className='home__container-catalog' >
+                    <h2 className='home__catalog-title' id="news">Новости</h2>
+                    <Category />
 
-                        {isLoading ? (
-                            <div style={{ display: 'flex', justifyContent: 'center', margin: '100px 0', height: '100vh' }}>
-                                <Spinner animation="border" role="status">
-                                    <span className="visually-hidden">Loading...</span>
-                                </Spinner>
-                            </div>
+                    {isLoading ? (
+                        <div style={{ display: 'flex', justifyContent: 'center', margin: '100px 0', height: '100vh' }}>
+                            <Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner>
+                        </div>
+                    ) : (
+                        productList && productList.length > 0 ? (
+                            <ProductList productList={productList} isLoading={isLoading} />
                         ) : (
-                            productList && productList.length > 0 ? (
-                                <ProductList productList={productList} isLoading={isLoading} />
-                            ) : (
-                                <div className='home__catalog-title' style={{ margin: '100px 0' }}>
-                                    No products available
-                                </div>
-                            )
-                        )}
-                    </div>
+                            <div className='home__catalog-title' style={{ margin: '100px 0' }}>
+                                No products available
+                            </div>
+                        )
+                    )}
                 </div>
-            </section>
-        </>
+            </div>
+        </section>
     );
 };
 
