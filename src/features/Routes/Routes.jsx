95fac90255ed36d94900ac9f6b84bb6a883e00ee
Lxg1c <teran_2005@mail.ru>
2025-03-31 16:50:27 +0300

    changed route to favoritePage

--- src/features/Routes/Routes.jsx
+++ src/features/Routes/Routes.jsx
@@ -4,7 +4,7 @@ import Cart from "../../pages/CartPage/Cart.jsx";
 import Auth from "../../pages/AuthPage/Auth.jsx";
 import { ROUTES } from "../../shared/lib/const.js";
 import ProductCard from "../ProductCard/ProductCard.jsx";
-import Favorite from "../../pages/FavoriteProductPage/Favorite.jsx";
+import Favorite from "../../pages/FavoritePage/Favorite.jsx";
 import About from "../../pages/AboutPage/About.jsx";
 
 const AppRoutes = () => {
