82cf66d8a1430785bdeb8803c292dbe7c06b6ff5
Lxg1c <teran_2005@mail.ru>
2025-03-31 16:48:51 +0300

    changed base_url_api

--- src/shared/lib/const.js
+++ src/shared/lib/const.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL_API = 'http://localhost:8000/api/v1';
+export const BASE_URL_API = 'http://localhost:8000';
 
 export const ROUTES = {
     CART: '/cart',
