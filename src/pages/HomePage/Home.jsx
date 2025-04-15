e973117304619dc85204791e7432494656989bc2
Lxg1c <teran_2005@mail.ru>
2025-04-15 18:30:35 +0300

    refactored: by prettier and eslint

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,51 +1,58 @@
-import { useEffect } from 'react';
-import './Home.scss';
-import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import ProductList from '../../widgets/ProductList/ProductList.jsx';
-import Category from '../../features/Category/Category.jsx';
-import Spinner from 'react-bootstrap/Spinner';
+import { useEffect } from "react";
+import "./Home.scss";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import ProductList from "../../widgets/ProductList/ProductList.jsx";
+import Category from "../../features/Category/Category.jsx";
+import Spinner from "react-bootstrap/Spinner";
 
 const Home = () => {
-    const productList = useSelector(state => state.products.filtered);
-    const isLoading = useSelector(state => state.products.isLoading);
-    const location = useLocation();
+  const productList = useSelector((state) => state.products.filtered);
+  const isLoading = useSelector((state) => state.products.isLoading);
+  const location = useLocation();
 
-    useEffect(() => {
-        if (location.hash) {
-            const element = document.getElementById(location.hash.slice(1));
-            if (element) {
-                element.scrollIntoView({ behavior: 'smooth' });
-            }
-        }
-    }, [location]);
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.slice(1));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
 
-    return (
-        <section className='home'>
-            <div className='home__container'>
-                <div className='home__container-catalog' >
-                    <h2 className='home__catalog-title' id="news">Новости</h2>
-                    <Category />
+  return (
+    <section className="home">
+      <div className="home__container">
+        <div className="home__container-catalog">
+          <h2 className="home__catalog-title" id="news">
+            Новости
+          </h2>
+          <Category />
 
-                    {isLoading ? (
-                        <div style={{ display: 'flex', justifyContent: 'center', margin: '100px 0', height: '100vh' }}>
-                            <Spinner animation="border" role="status">
-                                <span className="visually-hidden">Loading...</span>
-                            </Spinner>
-                        </div>
-                    ) : (
-                        productList && productList.length > 0 ? (
-                            <ProductList productList={productList} isLoading={isLoading} />
-                        ) : (
-                            <div className='home__catalog-title' style={{ margin: '100px 0' }}>
-                                No products available
-                            </div>
-                        )
-                    )}
-                </div>
+          {isLoading ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                margin: "100px 0",
+                height: "100vh",
+              }}
+            >
+              <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
             </div>
-        </section>
-    );
+          ) : productList && productList.length > 0 ? (
+            <ProductList productList={productList} isLoading={isLoading} />
+          ) : (
+            <div className="home__catalog-title" style={{ margin: "100px 0" }}>
+              No products available
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
