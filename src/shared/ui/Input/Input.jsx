2f2303614de11ecf2c899b403dff0432c368c57b
Lxg1c <teran_2005@mail.ru>
2025-04-15 18:30:16 +0300

    refactored: by prettier and eslint

--- src/shared/ui/Input/Input.jsx
+++ src/shared/ui/Input/Input.jsx
@@ -1,27 +1,27 @@
-import PropTypes from 'prop-types';
-import './Input.scss';
+import PropTypes from "prop-types";
+import "./Input.scss";
 
-const Input = ({ placeholder, onChange, name, disabled = false, type = 'text' }) => {
-    return (
-        <input
-            type={type}
-            name={name}
-            onChange={onChange}
-            placeholder={placeholder}
-            disabled={disabled}
-            className="input"
-        />
-    );
+const Input = ({ placeholder, onChange, name, disabled = false, type = "text" }) => {
+  return (
+    <input
+      type={type}
+      name={name}
+      onChange={onChange}
+      placeholder={placeholder}
+      disabled={disabled}
+      className="input"
+    />
+  );
 };
 
 Input.propTypes = {
-    placeholder: PropTypes.string,
-    name: PropTypes.string,
-    onChange: PropTypes.func,
-    disabled: PropTypes.bool,
-    type: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
+  type: PropTypes.string,
 };
 
-Input.displayName = 'Input';
+Input.displayName = "Input";
 
 export default Input;
