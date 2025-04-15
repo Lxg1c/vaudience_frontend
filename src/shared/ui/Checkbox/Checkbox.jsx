4c301f4ba58d6348154c36d647b39ede413c8c19
Lxg1c <teran_2005@mail.ru>
2025-04-15 18:30:50 +0300

    refactored: by prettier and eslint

--- src/shared/ui/Checkbox/Checkbox.jsx
+++ src/shared/ui/Checkbox/Checkbox.jsx
@@ -1,27 +1,27 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./Checkbox.scss";
 
 const Checkbox = ({ id, checked, onChange, disabled }) => {
-    return (
-        <label className="checkbox-container">
-            <input
-                className="checkbox-input"
-                id={id}
-                type="checkbox"
-                checked={checked}
-                onChange={onChange}
-                disabled={disabled}
-            />
-            <span className="checkbox-custom"></span>
-        </label>
-    );
+  return (
+    <label className="checkbox-container">
+      <input
+        className="checkbox-input"
+        id={id}
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <span className="checkbox-custom"></span>
+    </label>
+  );
 };
 
 Checkbox.propTypes = {
-    id: PropTypes.string,
-    checked: PropTypes.bool.isRequired,
-    onChange: PropTypes.func.isRequired,
-    disabled: PropTypes.bool,
+  id: PropTypes.string,
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
 };
 
 export default Checkbox;
