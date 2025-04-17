3c25ffeb6219a8d88ac3a21712554c5b4cdc059e
Lxg1c <teran_2005@mail.ru>
2025-04-17 20:21:11 +0300

    refactored: refactor imports

--- src/features/Category/Category.jsx
+++ src/features/Category/Category.jsx
@@ -1,46 +1,52 @@
-import './Category.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { filterByCategory, resetFilter } from '../../app/store/slices/productSlice.js';
-import { useEffect, useState } from 'react';
+import "./Category.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { useCallback, useEffect } from "react";
+import { setActiveCategory } from "@/enteties/category";
+import { filterByCategory, resetFilter } from "@/enteties/product/model/productSlice.js";
 
 const Category = () => {
-    const dispatch = useDispatch();
-    const categories = useSelector(state => state.categories.list);
-    const [activeCategory, setActiveCategory] = useState(null);
-    // const isLoading = useSelector(state => state.products.isLoading);
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories.list);
+  const activeCategory = useSelector((state) => state.categories.activeCategory);
 
-    useEffect(() => {
-        handleClearFilter();
-    }, []);
+  const handleClearFilter = useCallback(() => {
+    dispatch(resetFilter());
+    dispatch(setActiveCategory(null));
+  }, [dispatch]);
 
-    const handleCategoryClick = (category) => {
-        const cat = category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
-        dispatch(filterByCategory(cat));
-        setActiveCategory(category);
-    };
+  useEffect(() => {
+    handleClearFilter();
+  }, [handleClearFilter]);
 
-    const handleClearFilter = () => {
-        dispatch(resetFilter());
-        setActiveCategory(null);
-    };
+  const handleCategoryClick = (categoryId) => {
+    dispatch(filterByCategory(categoryId));
+    dispatch(setActiveCategory(categoryId));
+  };
 
-    return (
-        <div className="category">
-            <div className='category__container container'>
-                <ul className='category__container-list'>
-                    {categories.map((category, index) => (
-                        <li
-                            key={index}
-                            className={`category__container-list--item ${activeCategory === category.name ? 'active' : ''}`}
-                            onClick={() => handleCategoryClick(category.name)}
-                        >
-                            <button className='btn-reset'>{category.name.toUpperCase()}</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="category">
+      <div className="category__container container">
+        <ul className="category__container-list">
+          {categories.map((category) => (
+            <li
+              key={category.id}
+              className="category__container-list--item"
+              onClick={() => handleCategoryClick(category.id)}
+            >
+              <button
+                className={`btn-reset ${activeCategory === category.id ? "active-category" : ""}`}
+              >
+                {category.name.toUpperCase()}
+              </button>
+            </li>
+          ))}
+          <li className="category__container-list--item" onClick={handleClearFilter}>
+            <button className="btn-reset">ОЧИСТИТЬ</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
