b383e38b4933a16417269cae56870f23fc6b9b55
Lxg1c <teran_2005@mail.ru>
2025-04-17 11:15:11 +0300

    refactored: refactored import

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../../pages/HomePage/Home.jsx";
-import Cart from "../../pages/CartPage/Cart.jsx";
-import Auth from "../../pages/AuthPage/Auth.jsx";
-import { ROUTES } from "../../shared/lib/const.js";
-import ProductCard from "../ProductCard/ProductCard.jsx";
-import Favorite from "../../pages/FavoritePage/Favorite.jsx";
-import About from "../../pages/AboutPage/About.jsx";
-import Profile from "../../pages/ProfilePage/Profile.jsx";
+import Home from "@/pages/HomePage/Home.jsx";
+import Cart from "@/pages/CartPage/Cart.jsx";
+import Auth from "@/pages/AuthPage/Auth.jsx";
+import { ROUTES } from "@/shared/lib/const.js";
+import ProductCard from "@/features/ProductCard/ProductCard.jsx";
+import Favorite from "@/pages/FavoritePage/Favorite.jsx";
+import About from "@/pages/AboutPage/About.jsx";
+import Profile from "@/pages/ProfilePage/Profile.jsx";
 
 const AppRoutes = () => {
   return (
