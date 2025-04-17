aaba6670cff618df9db42f7fe955d4e7de07bb36
Lxg1c <teran_2005@mail.ru>
2025-04-17 20:18:29 +0300

    feat: import BASE URL API

--- src/enteties/category/api/api.js
+++ src/enteties/category/api/api.js
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { BASE_URL_API } from "@/shared/lib";
 
 export const getCategories = createAsyncThunk("categories/getCategories", async (_, thunkAPI) => {
   try {
-    const response = await axios.get("http://localhost:8000/api/v1/category/");
+    const response = await axios.get(`${BASE_URL_API}/api/v1/category/`);
     return response.data;
   } catch (error) {
     console.log(error);
