4a5c948069afd48476e33764a68bb2e429619805
Lxg1c <teran_2005@mail.ru>
2025-04-17 11:01:39 +0300

    refactored: by prettier and eslint

--- src/pages/FavoritePage/Favorite.jsx
+++ src/pages/FavoritePage/Favorite.jsx
@@ -1,54 +1,74 @@
-import deleteIcon from '../../assets/delete.svg';
+import deleteIcon from "@/assets/delete.svg";
 import "./Favorite.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { removeItemFromFavorite } from "../../app/store/slices/userSlice.js";
+import { removeItemFromFavorite } from "@/enteties/user/model/userSlice.js";
 import { Link } from "react-router-dom";
+import Button from "@/shared/ui/Button/Button.jsx";
 
 const Favorite = () => {
-    const dispatch = useDispatch();
-    const favorite = useSelector(state => state.user.favorite);
+  const dispatch = useDispatch();
+  const favorite = useSelector((state) => state.user.favorite);
 
-    const handleDelete = (product) => {
-        dispatch(removeItemFromFavorite({ id: product.id, size: product.size }));
-    };
+  const handleDelete = (product) => {
+    dispatch(removeItemFromFavorite({ id: product.id, size: product.size }));
+  };
 
-    return (
-        <div className="favorite__container container">
-            {favorite.length === 0 ? (
-                <div className="favorite__empty">
-                    <h2>Ваш список избранного пуст</h2>
-                    <Link to="/" className="favorite__empty-link">Вернуться к покупкам</Link>
+  return (
+    <div className="favorite">
+      <div className="favorite__container container">
+        {favorite.length === 0 ? (
+          <div className="favorite__empty">
+            <h2 className="favorite__empty-title">Ваш список избранного пуст</h2>
+            <Link to="/" className="favorite__empty-link">
+              Вернуться к покупкам
+            </Link>
+          </div>
+        ) : (
+          <ul className="favorite__list">
+            {favorite.map((product) => (
+              <li key={`${product.id}-${product.size}`} className="favorite__item">
+                <div className="favorite__item-content">
+                  <div className="favorite__item-info">
+                    <img
+                      src={
+                        product.images.find((img) => img.is_primary)?.url || product.images[0]?.url
+                      }
+                      className="favorite__item-img"
+                      alt={product.name}
+                    />
+                    <div className="favorite__item-details">
+                      <h3 className="favorite__item-title">{product.name}</h3>
+                      <p className="favorite__item-size">Размер: {product.size}</p>
+                    </div>
+                  </div>
+
+                  <div className="favorite__item-price">{product.price} ₽</div>
+
+                  <div className="favorite__item-btns">
+                    <button
+                      className="favorite__item-delete"
+                      onClick={() => handleDelete(product)}
+                      aria-label="Удалить из избранного"
+                    >
+                      <img
+                        src={deleteIcon}
+                        alt="Удалить из избранного"
+                        className="favorite__delete-icon"
+                      />
+                    </button>
+
+                    <Link to={`/products/${product.id}`}>
+                      <Button text="Выбрать размер" />
+                    </Link>
+                  </div>
                 </div>
-            ) : (
-                <ul className='favorite__list'>
-                    {favorite.map((product) => (
-                        <li key={product.id} className='favorite__item'>
-                            <Link to={`/products/${product.id}`} key={product.id} className="product__link">
-                                <div className='favorite__item'>
-                                    <img src={product.category.image} className='favorite__item-img' alt={product.name}/>
-                                    <div className='favorite__item-details'>
-                                        <div>
-                                            <h3 className='favorite__item-title favorite__title'>{product.title}</h3>
-                                        </div>
-
-                                        <div className='favorite__item-price-wrapper'>
-                                            <h1 className='favorite__item-price favorite__price'>{product.price * 100} ₽</h1>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Link>
-                            <button
-                                className='btn-reset favorite__item-btn--delete'
-                                onClick={() => handleDelete(product)}
-                            >
-                                <img src={deleteIcon} alt='Удалить товар'/>
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Favorite;
--- src/shared/lib/const.js
+++ src/shared/lib/const.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
-export const BASE_URL_API = 'http://localhost:8000';
+export const BASE_URL_API = "http://localhost:8000";
 
 export const ROUTES = {
-    CART: '/cart',
-    LOGIN: '/login',
-    REGISTER: '/register',
-    PRODUCT: '/products/:id',
-    FAVORITE: '/bookmark',
-    ABOUT: '/about',
-}
+  CART: "/cart",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  PRODUCT: "/products/:id",
+  FAVORITE: "/bookmark",
+  ABOUT: "/about",
+  PROFILE: "/profile",
+};
 
 export const api = axios.create({
-    baseURL: `${BASE_URL_API}`,
+  baseURL: `${BASE_URL_API}`,
 });
 
 api.interceptors.response.use(
