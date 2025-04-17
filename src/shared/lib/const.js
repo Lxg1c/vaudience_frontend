d434577536333237e18ddb2f2f4d577b3df0bc0d
Lxg1c <teran_2005@mail.ru>
2025-04-17 11:02:27 +0300

    deleted: deleted unused func

--- src/shared/lib/const.js
+++ src/shared/lib/const.js
@@ -15,34 +15,3 @@ export const ROUTES = {
 export const api = axios.create({
   baseURL: `${BASE_URL_API}`,
 });
-
-api.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
-
-        if (error.response.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
-
-            try {
-                await refreshAccessToken();
-
-                return api(originalRequest);
-            } catch (refreshError) {
-                return Promise.reject(refreshError);
-            }
-        }
-
-        return Promise.reject(error);
-    }
-);
-
-const refreshAccessToken = async () => {
-    try {
-        const response = await axios.post(`${BASE_URL_API}/auth/refresh/`);
-        return response.data;
-    } catch (error) {
-        console.error('Failed to refresh token:', error.message);
-        throw error;
-    }
-};
