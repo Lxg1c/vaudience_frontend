a5f768db3a81010b1edb352298d205217ab92973
Lxg1c <teran_2005@mail.ru>
2025-04-18 17:40:08 +0300

    feat: add cart and favorite

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer, productApi } from "@/enteties/product";
 import { categoryReducer } from "@/enteties/category";
 import { userReducer } from "@/enteties/user";
+import { cartReducer } from "@/enteties/cart/index.js";
+import { favoriteReducer } from "@/enteties/favorite/index.js";
 
 export const store = configureStore({
   reducer: {
     categories: categoryReducer,
     products: productReducer,
     user: userReducer,
+    cart: cartReducer,
+    favorite: favoriteReducer,
     [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productApi.middleware),
