5d301fd546d4e7d33d92a3252c53acf497d208c2
Lxg1c <teran_2005@mail.ru>
2025-04-19 16:54:29 +0300

    fix: replaced actions

--- src/features/ProductCard/ProductCard.jsx
+++ src/features/ProductCard/ProductCard.jsx
@@ -2,27 +2,25 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./ProductCard.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { Carousel, Spinner } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import "@/shared/ui/Button/Button.scss";
 import { notification } from "antd";
 import { useGetProductQuery } from "@/enteties/product/model/productApi.js";
 import { addProductToCart } from "@/enteties/cart/index.js";
-import { addItemToFavorite, removeItemFromFavorite } from "@/enteties/favorite/index.js";
 import Loader from "@/shared/ui/Loader/Loader.jsx";
+import {addToFavorite, removeFromFavorite} from "@/enteties/favorite/index.js";
 
 const ProductCard = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const navigate = useNavigate();
-
   const [productCount, setProductCount] = useState(1);
   const [selectedSize, setSelectedSize] = useState(null);
-
   const { data, error, isLoading } = useGetProductQuery(id);
-  const { currentUser, favorite } = useSelector(({ user }) => user);
-  const isFavorite = favorite?.some((item) => item.id === data.id && item.size === selectedSize);
-
+  const favorite = useSelector(state => state.favorite.favorite);
+  const currentUser = useSelector(state => state.user.currentUser);
   const [api, contextHolder] = notification.useNotification();
+  const isFavorite = data && favorite?.some((item) => item.product_id === data.id);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -90,16 +88,14 @@ const ProductCard = () => {
       return;
     }
 
+
+
     if (isFavorite) {
-      dispatch(removeItemFromFavorite({ id: data.id, size: selectedSize }));
-      showNotification("success", "Удалено из избранного", `${data.title} больше не в избранном`);
+      dispatch(removeFromFavorite({ user_id: currentUser.id, product_id: data.id }));
+      showNotification("success", "Удалено из избранного", `${data.name} больше не в избранном`);
     } else {
-      if (!selectedSize) {
-        showNotification("warning", "Выберите размер", "Пожалуйста, укажите размер товара");
-        return;
-      }
-      dispatch(addItemToFavorite({ ...data, size: selectedSize }));
-      showNotification("success", "Добавлено в избранное", `${data.title} теперь в избранном`);
+      dispatch(addToFavorite({ user_id: currentUser.id, product_id: data.id }));
+      showNotification("success", "Добавлено в избранное", `${data.name} теперь в избранном`);
     }
   };
 
@@ -158,7 +154,6 @@ const ProductCard = () => {
                 <button
                   className="btn-reset productCard__info-favorite"
                   onClick={toggleFavorite}
-                  disabled={!selectedSize}
                 >
                   <svg
                     width="40px"
@@ -166,7 +161,7 @@ const ProductCard = () => {
                     viewBox="0 0 24 24"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
-                    className={isFavorite ? "favorite" : "not-favorite"}
+                    className={isFavorite ? "favorite-icon" : "not-favorite-icon"}
                   >
                     <path
                       d="M6 7.2002V16.6854C6 18.0464 6 18.7268 6.20412 19.1433C6.58245 19.9151 7.41157 20.3588 8.26367 20.2454C8.7234 20.1842 9.28964 19.8067 10.4221 19.0518L10.4248 19.0499C10.8737 18.7507 11.0981 18.6011 11.333 18.5181C11.7642 18.3656 12.2348 18.3656 12.666 18.5181C12.9013 18.6012 13.1266 18.7515 13.5773 19.0519C14.7098 19.8069 15.2767 20.1841 15.7364 20.2452C16.5885 20.3586 17.4176 19.9151 17.7959 19.1433C18 18.7269 18 18.0462 18 16.6854V7.19691C18 6.07899 18 5.5192 17.7822 5.0918C17.5905 4.71547 17.2837 4.40973 16.9074 4.21799C16.4796 4 15.9203 4 14.8002 4H9.2002C8.08009 4 7.51962 4 7.0918 4.21799C6.71547 4.40973 6.40973 4.71547 6.21799 5.0918C6 5.51962 6 6.08009 6 7.2002Z"
@@ -181,7 +176,7 @@ const ProductCard = () => {
               </div>
             </div>
 
-            <h2 className="productCard__info-subtitle">{data.title}</h2>
+            <h2 className="productCard__info-subtitle">{data.name}</h2>
 
             {Array.isArray(data.description) ? (
               <ul className="productCard__info-description">
