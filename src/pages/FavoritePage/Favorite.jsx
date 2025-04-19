078d7507e47967986a097c3bd9642f2a676abab7
Lxg1c <teran_2005@mail.ru>
2025-04-19 16:53:34 +0300

    feat: replaced by favoriteItem

--- src/pages/FavoritePage/Favorite.jsx
+++ src/pages/FavoritePage/Favorite.jsx
@@ -1,17 +1,10 @@
-import deleteIcon from "@/assets/delete.svg";
 import "./Favorite.scss";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Button from "@/shared/ui/Button/Button.jsx";
-import { removeItemFromFavorite } from "@/enteties/favorite/index.js";
+import FavoriteItem from "@/pages/FavoritePage/FavoriteItem.jsx";
 
 const Favorite = () => {
-  const dispatch = useDispatch();
-  const favorite = useSelector((state) => state.user.favorite);
-
-  const handleDelete = (product) => {
-    dispatch(removeItemFromFavorite({ id: product.id, size: product.size }));
-  };
+  const favorite = useSelector((state) => state.favorite.favorite);
 
   return (
     <div className="favorite">
@@ -26,43 +19,7 @@ const Favorite = () => {
         ) : (
           <ul className="favorite__list">
             {favorite.map((product) => (
-              <li key={`${product.id}-${product.size}`} className="favorite__item">
-                <div className="favorite__item-content">
-                  <div className="favorite__item-info">
-                    <img
-                      src={
-                        product.images.find((img) => img.is_primary)?.url || product.images[0]?.url
-                      }
-                      className="favorite__item-img"
-                      alt={product.name}
-                    />
-                    <div className="favorite__item-details">
-                      <h3 className="favorite__item-title">{product.name}</h3>
-                      <p className="favorite__item-size">Размер: {product.size}</p>
-                    </div>
-                  </div>
-
-                  <div className="favorite__item-price">{product.price} ₽</div>
-
-                  <div className="favorite__item-btns">
-                    <button
-                      className="favorite__item-delete"
-                      onClick={() => handleDelete(product)}
-                      aria-label="Удалить из избранного"
-                    >
-                      <img
-                        src={deleteIcon}
-                        alt="Удалить из избранного"
-                        className="favorite__delete-icon"
-                      />
-                    </button>
-
-                    <Link to={`/products/${product.id}`}>
-                      <Button text="Выбрать размер" />
-                    </Link>
-                  </div>
-                </div>
-              </li>
+              <FavoriteItem key={product.id} favoriteItem={product} />
             ))}
           </ul>
         )}
